6ec2f015078103beb078e803b29fe443bac481e8
Blair Lunceford <blair@workos.com>
2021-11-04 12:07:11 -0600

    Update SDK version and fix errors

--- ruby-directory-sync-example/app.rb
+++ ruby-directory-sync-example/app.rb
@@ -9,28 +9,33 @@ require 'json'
 WorkOS.key = ENV['WORKOS_API_KEY']
 
 get '/' do
-  @directories = WorkOS::DirectorySync.list_directories
+  @directories_list = WorkOS::DirectorySync.list_directories
+  @directories = @directories_list.data
 
   erb :index
 end
 
 get '/directories/:id' do
-  @groups = WorkOS::DirectorySync.list_groups(directory: params[:id])
-  @users = WorkOS::DirectorySync.list_users(directory: params[:id])
+  @groups_list = WorkOS::DirectorySync.list_groups(directory: params[:id])
+  @groups = @groups_list.data
+  @users_list = WorkOS::DirectorySync.list_users(directory: params[:id])
+  @users = @users_list.data
 
   erb :directory
 end
 
 get '/users/:id' do
   @user = WorkOS::DirectorySync.get_user(params[:id])
-  @user_groups = WorkOS::DirectorySync.list_groups(user: params[:id])
+  @user_groups_list = WorkOS::DirectorySync.list_groups(user: params[:id])
+  @user_groups = @user_groups_list.data
 
   erb :user
 end
 
 get '/groups/:id' do
   @group = WorkOS::DirectorySync.get_group(params[:id])
-  @group_users = WorkOS::DirectorySync.list_users(group: params[:id])
+  @group_users_list = WorkOS::DirectorySync.list_users(group: params[:id])
+  @group_users = @group_users_list.data
 
   erb :group
 end
