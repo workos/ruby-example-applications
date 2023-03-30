aa20ad4fdc05b2bd62b1dfdb9db6b927e3d6ae94
Adam <adam.zinder@workos.com>
2023-03-30 12:39:00 -0700

    updates package versions in gemfiles, readmes and directory sync app

--- ruby-directory-sync-example/app.rb
+++ ruby-directory-sync-example/app.rb
@@ -6,7 +6,6 @@ require 'sinatra'
 require 'workos'
 require 'json'
 require 'sinatra-websocket'
-require 'pry'
 
 set :server, 'thin'
 set :sockets, []
@@ -14,12 +13,38 @@ set :sockets, []
 # Pull API key from ENV variable
 WorkOS.key = ENV['WORKOS_API_KEY']
 
-get '/' do
-  @directories_list = WorkOS::DirectorySync.list_directories
-  @directories = @directories_list.data
+enable :sessions
 
+use(
+  Rack::Session::Cookie,
+  key: 'rack.session',
+  domain: 'localhost',
+  path: '/',
+  expire_after: 2_592_000,
+  secret: SecureRandom.hex(16)
+)
+
+get '/' do
+  before = params[:before]
+  after = params[:after]
+  puts before
+  puts after 
+  if !before
+    @directories = WorkOS::DirectorySync.list_directories(
+      limit: 5
+    )
+  else
+    @directories = WorkOS::DirectorySync.list_directories(
+      limit: 5,
+      before: before,
+      after: after
+    )
+  end
+  @before = @directories.list_metadata["before"]
+  @after = @directories.list_metadata["after"]
   erb :index, :layout => false
 end
+
 
 get '/directories/:id' do
   @groups_list = WorkOS::DirectorySync.list_groups(directory: params[:id])
@@ -32,7 +57,7 @@ end
 
 get '/users/:id' do
   @user = WorkOS::DirectorySync.get_user(params[:id])
-  @user_groups_list = WorkOS::DirectorySync.list_groups(user: params[:id])
+  @user_groups_list = WorkOS::DirectorySync.list_groups(user: params[:id], limit: 5)
   @user_groups = @user_groups_list.data
 
   erb :user
